e98e20b3181070d578973388679d4b8e8c04a35e
Teerth <Teerth5121@gmail.com>
2026-08-24 20:38:21 +0530

    Added Ledger app, slides are closable and stackable. Updated other things

--- script.js
+++ script.js
@@ -1,21 +1,45 @@
 console.log("working");
 
-const closeBtn = document.getElementById("closeBtn");
+const header = document.getElementById("header");
+const closeFirstSlideBtn = document.getElementById("closeFirstSlideBtn");
+const closeLedgerSlideBtn = document.getElementById("closeLedgerSlideBtn");
 const desktopBtn = document.getElementById("deskBtn");
+const ledgerBtn = document.getElementById("noteBtn");
 const firstWindow = document.getElementById("firstWindow");
+const ledgerSlide = document.getElementById("ledgerSlide");
 
 function closeWindow(element) {
     element.style.visibility="hidden";   
 }
 function openWindow(element) {
     element.style.visibility = "visible";
+    biggestIndex++;
+    element.style.zIndex=biggestIndex;
+    header.style.zIndex=biggestIndex+1;
+}
+function selectedBtn(element) {
+    if (element.classList.contains("selected")) {
+        element.classList.remove("selected");
+    } else {
+        element.classList.add("selected");
+    }
 }
 
-closeBtn.addEventListener("click", ()=>closeWindow(firstWindow));
+closeFirstSlideBtn.addEventListener("click", ()=>closeWindow(firstWindow));
+closeLedgerSlideBtn.addEventListener("click", ()=>{
+    closeWindow(ledgerSlide);
+    selectedBtn(ledgerBtn);
+});
 desktopBtn.addEventListener("click", ()=>openWindow(firstWindow));
+ledgerBtn.addEventListener("click", ()=>{
+    openWindow(ledgerSlide);
+    selectedBtn(ledgerBtn);
+});
 
-
+addTopHandling(firstWindow);
+addTopHandling(ledgerSlide);
 dragElement(firstWindow);
+dragElement(ledgerSlide);
 
 function dragElement(elemnt) {
     var pos1=0, pos2=0, pos3=0, pos4=0;
@@ -72,3 +96,41 @@ async function getEmoQuo() {
     emojiPlace.innerHTML = emoji;
 }
 getEmoQuo();
+
+// slidePositionHandling
+var biggestIndex=1;
+
+function addTopHandling(element) {
+    element.addEventListener("mousedown",()=> handleTop(element));
+}
+
+function handleTop(element) {
+    biggestIndex++;
+    element.style.zIndex=biggestIndex;
+    header.style.zIndex=biggestIndex+1;
+}
+
+// ledger app logic
+const departBtn = document.querySelector("#departBtn");
+const table = document.querySelector("#ledgerTable");
+
+function depart(button) {
+    button.style.visibility="hidden";
+    let row = button.parentNode.parentNode;
+    row.parentNode.removeChild(row);
+}
+
+function addGuest() {
+    let name = prompt("What ye called?");
+    let place = prompt("Where ye from?");
+    let entry = prompt("When hath thee joined?");
+    let exit = prompt("When shall thee departeth?");
+
+    let newRow = table.insertRow(table.rows.length);
+
+    newRow.insertCell(0).innerHTML=name;
+    newRow.insertCell(1).innerHTML=place;
+    newRow.insertCell(2).innerHTML=entry;
+    newRow.insertCell(3).innerHTML=exit;
+    newRow.insertCell(4).innerHTML='<p class="btn departBtn" onclick="depart(this)">Aye</p>';
+}
